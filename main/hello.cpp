778803c80a78558ec23c618c5b36660f3eb7cb4f
PES1UG20CS325 <125734220+PES1UG20CS325@users.noreply.github.com>
2023-02-18 18:13:00 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
+  cout << "PES UNIVERSITY\n";
   cout << "I have successfully built and run\n";
   cout << "PES1UG20CS325 - PRANEETH\n";
   return 0;
